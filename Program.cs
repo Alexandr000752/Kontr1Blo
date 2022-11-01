6237502da5be25bbbb70a03f83c5431d584008f6
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-11-01 20:34:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите цифрой размер массива: ");
+int n = int.Parse(Console.ReadLine());
+
+string[] array = new string[n];
+
+for(int index = 0; index < n; index++)
+{
+	Console.Write($"Введите {index+1} значение: ");
+	array[index] = Console.ReadLine();
+}
+
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+		Console.Write(array[i] + ", ");
+}
+Console.Write("] -> ");
+
+
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+	if(array[i].Length <= 3)
+	{
+		Console.Write(array[i] + ", ");
+		
+	}
+}
+Console.Write("]");
